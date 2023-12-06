a72317238503ee695b67531f0958bf0b33925ecc
Maria Bekish <masha.bekish@gmail.com>
2023-12-06 12:29:38 +0300

    Add ASP.NET Core MVC project with Individual Accounts authentication

---
+++ HolidayWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HolidayWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
